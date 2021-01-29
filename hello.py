1606e3621de14332e46795b8a8c2b86d71dcfaf6
ssungs <asdf4651@naver.com>
2021-01-29 11:49:51 +0900

    feat: Add input() to get number from user
    and we use this num to for statement.

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 def print_hello():
-    """
-    print formatted string with i if i is even.
-    else, print i id odd.
-    """
-    for i in range(1, 10+1):
+
+    num = int(input('How many numbers do you want to print? '))
+    for i in range(1, num+1):
         if i%2==0:
             print('hello, git for {}th time(s)!'.format(i))
         else:
